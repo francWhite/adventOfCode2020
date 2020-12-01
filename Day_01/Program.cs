7a7a5eaf9053eb22c012d4272815e09e34db8a6f
Francisco <francisco.metzger@gmail.com>
2020-12-01 09:23:15 +0100

    solved Day01

--- Day_01/Program.cs
+++ Day_01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Day_01
 {
@@ -6,7 +8,30 @@ namespace Day_01
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = File
+				.ReadLines("./input.txt")
+				.Select(line => Convert.ToInt32(line))
+				.ToList();
+			
+			foreach (var number1 in input)
+			{
+				foreach (var number2 in input)
+				{
+					foreach (var number3 in input)
+					{
+						if (number1 == number2 && number2 == number3)
+						{
+							continue;
+						}
+
+						if (number1 + number2 + number3 == 2020)
+						{
+							Console.WriteLine($"Numbers found: {number1}, {number2}, {number3} result: {number1 * number2 * number3}");
+							return;
+						}
+					}
+				}
+			}
 		}
 	}
 }
