af384b22cafaeef89fafac56f2008ea1736355a1
Francisco <francisco.metzger@gmail.com>
2020-12-05 12:03:18 +0100

    solved Day_05 part 2

--- Day_05/Program.cs
+++ Day_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -11,12 +12,32 @@ namespace Day_05
 		static void Main(string[] args)
 		{
 			var input = File.ReadAllLines("./input.txt");
+			var seats = input.Select(SeatEvaluator.ParseAndEvaluate).ToList();
 
-			var maxSeatId = input
-				.Select(SeatEvaluator.ParseAndEvaluate)
-				.Max(seat => seat.CalculateSeatId());
-			
+			Part1(seats);
+			Part2(seats);
+		}
+
+		private static void Part1(IEnumerable<Seat> seats)
+		{
+			var maxSeatId = seats.Max(seat => seat.CalculateSeatId());
 			Console.WriteLine($"max seatId: {maxSeatId}");
+		}
+
+		private static void Part2(IReadOnlyList<Seat> seats)
+		{
+			var lastSeatId = seats.Min(s => s.CalculateSeatId());
+			foreach (var seat in seats.OrderBy(s => s.CalculateSeatId()))
+			{
+				var currentSeatId = seat.CalculateSeatId();
+				if (currentSeatId - lastSeatId > 1)
+				{
+					Console.WriteLine($"my seatId: {lastSeatId + 1}");
+					break;
+				}
+
+				lastSeatId = currentSeatId;
+			}
 		}
 	}
 }
